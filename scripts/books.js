1ec1c4529c235d80ae2db4545a2efb4770c3d199
Shail-26 <22cs035@charusat.edu.in>
2023-08-13 23:50:02 +0530

    Add 2 books and add it in javascript

---
+++ scripts/books.js
@@ -0,0 +1,14 @@
+const books = [{
+  id: 1,
+  image: '/images/Books Poster/It-Ends-With-Us.jpg',
+  pdf: '/Books Data/It-Ends-with-Us.pdf',
+  title: 'It Ends With Us',
+  author: 'Colleen Hoover',
+  isbn: '0140422161'
+}, {
+  id: 2,
+  image: '/images/Books Poster/It-Starts-With-Us.jpg',
+  pdf: '/Books Data/It-Starts-With-Us.pdf',
+  title: 'It Starts With Us',
+  author: 'Colleen Hoover',
+}];
