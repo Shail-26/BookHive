90c112512c97cd4c5ff6898e9a85678cbe467001
22CS036dhruvimahale <22cs036@charusat.edu.in>
2023-10-23 12:13:14 +0530

    More books added

--- scripts/books.js
+++ scripts/books.js
@@ -3,7 +3,7 @@ export let num = 0;
 export const books = [{
   id: ++num,
   image: "images/Book cover pages/8-rules-of-love.jpg",
-  pdf: "Books Data/",
+  pdf: "Books Data/8-rules-of-love.pdf",
   title: "8 Rules Of Love",
   author: "Jay Shetty",
   desc: "Instead of presenting love as an ethereal concept or a collection of cliches, Jay Shetty lays out specific, actionable steps to help you develop the skills to practice and nurture love better than ever before. He shares insights on how to win or lose together, how to define love, and why you dont break in a break-up. Inspired by Vedic wisdom and modern science, he tackles the entire relationship cycle, from first dates to moving in together to breaking up and starting over. And he shows us how to avoid falling for false promises and unfulfilling partners. By living Jay Shettys eight rules, we can all love ourselves, our partner, and the world better than we ever thought possible."
@@ -140,4 +140,109 @@ export const books = [{
   title: "Write me a love story",
   author: "Ravinder Singh",
   desc: " Write Me A Love Story by Ravinder Singh, is his latest romance novel set between a best selling author and a literary editor at a budding publishing house. Abhimanyu Razdan & Asmita Mullick starts of on a very wrong foot. Both of them are at each others throat since day one andnow Abhimanyu is all set ruin her career."
+}, {
+  id: ++num,
+  image: "images/Book cover pages/the-story-of-my-life.jpg",
+  pdf: "Books Data/the-story-of-my-life.pdf",
+  title: "The Story of My Life (1903)",
+  author: "Helen Keller",
+  desc: "The Story of My Life (1903) chronicles the early years of Helen Keller, a young woman who became both deaf and blind at a young age. The book explores the challenges she faced growing up as a child with disabilities, and introduces the amazing people who helped her along the way."
+}, {
+  id: ++num,
+  image: "images/Book cover pages/the-precipice.jpg",
+  pdf: "Books Data/the-precipice.pdf",
+  title: "The Precipice",
+  author: "Toby Ord",
+  desc: "The world is in the midst of what may be the most deadly pandemic of the past 100 years. Threats to humanity, and how we address them, define our time. Splendid . . . The Precipice is a powerful book, written with a philosophers eye . . . Ords love for humanity and hope for its future is infectious Spectator."
+}, {
+  id: ++num,
+  image: "images/Book cover pages/the-great-gatsby.jpg",
+  pdf: "Books Data/the-great-gatsby.pdf",
+  title: "The Great Gatsby",
+  author: "F. Scott Fitzgerald",
+  desc: "With an Introduction and Notes by Guy Reynolds, University of Kent at Canterbury. Generally considered to be F. Scott Fitzgeralds finest novel, The Great Gatsby is a consummate summary of the roaring twenties, and a devastating expose of the Jazz Age. Through the narration of Nick Carraway, the reader is taken into the superficially glittering world of the mansions which lined the Long Island shore in the 1920s, to encounter Nicks cousin Daisy, her brash but wealthy husband Tom Buchanan, Jay Gatsby and the mystery that surrounds him."
+}, {
+  id: ++num,
+  image: "images/Book cover pages/to-kill-a-mockingbird.jpg",
+  pdf: "Books Data/to-kill-a-mockingbird.pdf",
+  title: "To Kill a Mockingbird",
+  author: "Harper Lee",
+  desc: "The explosion of racial hate in an Alabama town is viewed by a little girl whose father defends a black man accused of rape."
+}, {
+  id: ++num,
+  image: "images/Book cover pages/harry-potter-and-the-sorcerers-stone.jpg",
+  pdf: "Books Data/harry-potter-and-the-sorcerers-stone.pdf",
+  title: "Harry Potter and the Sorcerers Stone Part - 1",
+  author: "J.K.Rowling",
+  desc: "In Harry Potter and the Sorcerers Stone, Harry discovers he is a wizard on his 11th birthday and is invited to attend Hogwarts School of Witchcraft and Wizardry. He learns about his parents tragic past and their connection to the dark wizard, Voldemort. Alongside his friends, Ron and Hermione, Harry uncovers the mystery of the Sorcerers Stone and faces Voldemorts attempt to regain immortality. The book introduces readers to the magical world, including Quidditch, the Hogwarts houses, and the importance of love and friendship in the battle between good and evil."
+}, {
+  id: ++num,
+  image: "images/Book cover pages/harry-potter-and-the-chamber-of-secrets.jpg",
+  pdf: "Books Data/harry-potter-and-the-chamber-of-secrets.pdf",
+  title: "Harry Potter and the Chamber of Secrets Part - 2",
+  author: "J.K.Rowling",
+  desc: "In Harry Potter and the Chamber of Secrets, Harry returns to Hogwarts for his second year, but a series of mysterious attacks petrify students. He uncovers the legend of the Chamber of Secrets, a hidden chamber within the school. With the help of Ron and Hermione, Harry delves into the history of the school and confronts a deadly basilisk and the memory of Tom Riddle, a young Voldemort. The book explores themes of prejudice against Muggle-borns and the consequences of discrimination. Harrys bravery saves the day, and he gains a better understanding of his connection to Voldemort through a shared ability to speak Parseltongue."
+}, {
+  id: ++num,
+  image: "images/Book cover pages/harry-potter-and-the-prisoner-of-azkaban.jpg",
+  pdf: "Books Data/harry-potter-and-the-prisoner-of-azkaban.pdf",
+  title: "Harry Potter and the Prisoner of Azkaban  Part - 3",
+  author: "J.K.Rowling",
+  desc: "In Harry Potter and the Prisoner of Azkaban, Harry learns that a dangerous prisoner Sirius Black has escaped from Azkaban and may be seeking him. He also discovers the truth about his parents friends, the Marauders, and learns about his godfather, Sirius. The book introduces the enigmatic Defense Against the Dark Arts teacher, Professor Lupin, and the malevolent Dementors. Harry and his friends unravel the mystery of the Marauders Map and the time-turner, ultimately revealing that Sirius is not a villain but a victim. The novel explores themes of friendship, betrayal, and the power of love and forgiveness, while deepening the backstory of Harrys parents and the ongoing struggle against the dark forces in the wizarding world."
+}, {
+  id: ++num,
+  image: "images/Book cover pages/harry-potter-and-the-goblet-of-fire.jpg",
+  pdf: "Books Data/harry-potter-and-the-goblet-of-fire.pdf",
+  title: "Harry Potter and the Goblet of Fire Part - 4",
+  author: "J.K.Rowling",
+  desc: "In Harry Potter and the Goblet of Fire, Harry is unexpectedly entered into the Triwizard Tournament, a dangerous competition for wizards. He faces deadly challenges alongside older students but uncovers a dark conspiracy when hes forced to retrieve the powerful Triwizard Cup. This book also delves into Voldemorts return to power, leading to a shocking confrontation. Themes of identity, prejudice, and the consequences of choices are explored as Harry faces increasing danger and loss."
+}, {
+  id: ++num,
+  image: "images/Book cover pages/harry-potter-and-the-order-of-phoenix.jpg",
+  pdf: "Books Data/harry-potter-and-the-order-of-phoenix.pdf",
+  title: "Harry Potter and the Order of the Phoenix Part - 5",
+  author: "J.K.Rowling",
+  desc: "In Harry Potter and the Order of the Phoenix, Harry returns to Hogwarts for his fifth year and forms a secret group, Dumbledores Army, to prepare students for battle against the dark wizard Voldemort. He learns about Voldemorts past and connection to him through Professor Snapes memories. The Ministry of Magic denies Voldemorts return, causing tension. The book culminates in a battle at the Department of Mysteries, where Harry and his friends face Death Eaters and the loss of his godfather, Sirius Black. The novel explores themes of adolescence, rebellion, and the importance of unity in the face of evil."
+}, {
+  id: ++num,
+  image: "images/Book cover pages/harry-potter-and-the-half-blood-prince.jpg",
+  pdf: "Books Data/harry-potter-and-the-half-blood-prince.pdf",
+  title: "Harry Potter and the Half-Blood Prince Part - 6",
+  author: "J.K.Rowling",
+  desc: "In Harry Potter and the Half-Blood Prince, Harry learns about the Horcruxes, objects containing pieces of Voldemorts soul, and his quest to destroy them. Dumbledore and Harry embark on a dangerous mission to retrieve one such object, but Dumbledore is killed by Snape. The book delves into Voldemorts dark past and his quest for immortality. Harry also struggles with his feelings for Ginny Weasley and Rons relationship with Hermione. The novel explores the theme of destiny and the sacrifices made in the fight against evil."
+}, {
+  id: ++num,
+  image: "images/Book cover pages/harry-potter-and-the-deathly-hallows.jpg",
+  pdf: "Books Data/harry-potter-and-the-deathly-hallows.pdf",
+  title: "Harry Potter and the Deathly Hallows Part - 7",
+  author: "J.K.Rowling",
+  desc: "In Harry Potter and the Deathly Hallows, Harry, Ron, and Hermione go on a quest to find and destroy Voldemorts Horcruxes, objects that contain fragments of his soul. The trio learns about the Deathly Hallows, powerful magical artifacts. They face numerous challenges and confrontations, leading to the epic Battle of Hogwarts. The book delves into the importance of love, self-sacrifice, and the power of choice in the final showdown between Harry and Voldemort. Themes of mortality, friendship, and the triumph of good over evil are central to this thrilling conclusion of the series."
+}, {
+  id: ++num,
+  image: "images/Book cover pages/rich-dad-poor-dad.jpg",
+  pdf: "Books Data/rich-dad-poor-dad.pdf",
+  title: "Rich Dad, Poor Dad",
+  author: "Robert T. Kiyosaki",
+  desc: "Rich Dad Poor Dad is a book by Robert T. Kiyosaki that contrasts financial philosophies of two father figures, one rich and one poor. The book emphasizes assets over liabilities and promotes financial education, the cashflow quadrant, and entrepreneurship for financial independence. It challenges traditional financial thinking."
+}, {
+  id: ++num,
+  image: "images/Book cover pages/do-epic-shit.jpg",
+  pdf: "Books Data/do-epic-shit.pdf",
+  title: "Do Epic Shit",
+  author: "Ankur Warikoo",
+  desc: "Do Epic Shit is a book by Ankur Warikoo that encourages readers to pursue remarkable and meaningful achievements in their lives. It inspires individuals to take bold actions, set ambitious goals, and overcome obstacles to make a significant impact. The book provides motivational insights and practical advice for personal and professional growth, urging readers to embrace challenges and strive for greatness."
+}, {
+  id: ++num,
+  image: "images/Book cover pages/cant-hurt-me.jpg",
+  pdf: "Books Data/cant-hurt-me.pdf",
+  title: "Cant Hurt Me: Master Your Mind and Defy the Odds",
+  author: "David Goggins",
+  desc: "Cant Hurt Me is an autobiography and self-help book by David Goggins, co-authored with Adam Skolnick. The book chronicles Goggins life journey from a challenging childhood to becoming a Navy SEAL and an ultra-endurance athlete. It explores his remarkable transformation, physical and mental toughness, and his relentless pursuit of self-improvement. The book emphasizes the power of mindset, resilience, and pushing ones limits to overcome adversity and achieve extraordinary goals. Goggins shares his personal experiences and offers readers practical techniques for developing mental strength and achieving their full potential."
+}, {
+  id: ++num,
+  image: "images/Book cover pages/the-subtle-art-of-not-giving-a-fuck.jpg",
+  pdf: "Books Data/the-subtle-art-of-not-giving-a-fuck.pdf",
+  title: "The Subtle Art of Not Giving a F_ck",
+  author: "Mark Manson",
+  desc: "The Subtle Art of Not Giving a F_ck is a self-help book by Mark Manson. It presents a no-nonsense approach to personal development and happiness. The book suggests that instead of trying to be endlessly positive or striving for constant success, individuals should focus on what truly matters to them and let go of superficial concerns. It encourages readers to embrace lifes inevitable struggles and challenges, accept their limitations, and choose where to invest their care and attention more thoughtfully. The books message is about finding meaning and purpose through honest self-assessment and prioritizing what truly matters in life."
 }];
